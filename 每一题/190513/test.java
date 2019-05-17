40c6acba57adf4b572fd81537efb68ca7dbff893
F-WJ <364580936@qq.com>
2019-05-17 11:42:28 +0800

    更新题目

---
+++ 每一题/190513/test.java
@@ -0,0 +1,26 @@
+package cn.onedull.practice._02;
+/**
+请问下面说法正确的是？
+A.列表（List)和集合（Set）中存放的元素都是可以重复的。
+B.映射（Map）<key,value>中key是可以重复的。
+C.映射（Map）<key,value>中value是可以重复的。
+D.列表（List)和集合（Set）中存放的元素都是不可以重复的。
+
+答案与解析
+
+1.相关知识
+列表（List）的元素有序、可重复
+集合（Set）的元素无序、不可重复
+映射（Map）的元素key不可以重复、value可以重复
+map是Set的扩展，相当于给不可重复的Set的每一个值绑定一个值value，value可以是任何值，因此可以重复
+
+2.答案解析
+A.Set不可以重复，错误。
+B.Map中key不可以重复，错误
+C.Map中value可以重复，正确
+D.List可以重复，错误
+答案选：C
+ */
+public class test {
+	
+}
