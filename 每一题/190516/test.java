552d72d67d671ff7d251e32b096d5b9bc84278b6
F-WJ <364580936@qq.com>
2019-05-19 23:52:44 +0800

    servlet知识点

---
+++ 每一题/190516/test.java
@@ -0,0 +1,24 @@
+package cn.onedull.practice._01;
+/**
+在Web应用程序的文件结构目录中，web.xml文件放在什么位置？
+A.WEB-INF目录
+B.conf目录
+C.lib
+D.classes目录
+
+答案与解析
+
+1.相关知识
+web.xm文件是用来初始化配置信息的，放在WEB-INF目录下
+/WEB-INF/classes用来存放编译后的.class文件
+WEB-INF/lib用来存放应用程序用到的jar包
+web应用程序不存在必须的conf目录
+
+2.答案解析
+根据相关知识可以知道，web.xml存放在WEB-INF目录下
+答案选：A
+
+ */
+public class test {
+	
+}
