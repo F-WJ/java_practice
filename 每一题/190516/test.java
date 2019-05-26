494a9741ad3ecfdd67c065b271aa8292892d2257
F-WJ <364580936@qq.com>
2019-05-26 23:42:55 +0800

    cookie和session

--- 每一题/190516/test.java
+++ 每一题/190516/test.java
@@ -1,24 +1,39 @@
-package cn.onedull.practice._01;
-/**
-在Web应用程序的文件结构目录中，web.xml文件放在什么位置？
-A.WEB-INF目录
-B.conf目录
-C.lib
-D.classes目录
+package cn.onedull.practice._03;
 
+/**
 答案与解析
 
-1.相关知识
-web.xm文件是用来初始化配置信息的，放在WEB-INF目录下
-/WEB-INF/classes用来存放编译后的.class文件
-WEB-INF/lib用来存放应用程序用到的jar包
-web应用程序不存在必须的conf目录
-
+1.相关知识返回值中++在前时，会先+1再返回
+返回值中++在后时，会先返回再+1
+静态变量为所有对象共享
 2.答案解析
-根据相关知识可以知道，web.xml存放在WEB-INF目录下
-答案选：A
-
+因为count为静态变量，因此所有对象访问的都为同一个变量
+首先count对象调用add方法，++在后，输出0，值为1
+调用add2方法，++在前，先+1，输出2，值为2
+调用count2的add方法，++在后，输出2，值变为3
+输出结果为 0 2 2
  */
 public class test {
+	public static void main(String[] args) {
+		Count count = new Count();
+		System.out.println(count.add() + " ");
+		System.out.println(count.add2() + " ");
+		Count count2 = new Count();
+		System.out.println(count2.add());
+	}
 	
+	
+}
+
+
+class Count{
+	private static int count = 0;
+	
+	public int add(){
+		return count++;
+	}
+	
+	int add2(){
+		return ++count;
+	}
 }
